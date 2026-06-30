9d94267a74a1e7cbef7291803bdf0a328c8b9b1c
ProgramerXYZ <194214968+ProgramerXYZ@users.noreply.github.com>
2026-06-30 05:46:55 +0530

    refactor: clean up code formatting and improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,70 @@
-
 use std::io;
-
 
 fn input() -> String {
     let mut text: String = String::new();
 
-    io::stdin()
-        .read_line(&mut text)
-        .unwrap();
+    io::stdin().read_line(&mut text).unwrap();
 
     text.trim().to_string()
 }
 
-fn parser(x:&str) -> Vec<&str>{
-    let it: Vec<&str> =x.split_whitespace().collect();
+fn parser(x: &str) -> Vec<&str> {
+    let it: Vec<&str> = x.split_whitespace().collect();
     it
-
-      
 }
-
-fn command_detechtor(x:&Vec<&str>)-> Result<i8, String>{
+//-----------------------------------------Command Detechtor-------------------------------------
+fn command_detechtor(x: &[&str]) -> Result<i8, String> {
     // let command = x[0];
-    let result:i8;
-    if &x[0] == &"add" {
-        result =1;
+    let result: i8;
+    if x[0] == "add" {
+        result = 1;
         Ok(result)
-    }
-    else if &x[0] == &"remove" || &x[0] == &"rm" {
+    } else if x[0] == "remove" || x[0] == "rm" {
         result = 2;
-       Ok(result)
-    }else if &x[0] == &"show" {
+        Ok(result)
+    } else if x[0] == "show" {
         result = 3;
         Ok(result)
-        
+    } else {
+        Err("unknown command".to_string())
     }
-    else {
-            Err("unknown command".to_string())
-        }
 }
-fn remove(what_to_remove:&String,the_vector_to_remove_from_the_save_var :&mut Vec<String>)-> String {
+//-----------------------------------------Remove-------------------------------------
+fn remove(
+    what_to_remove: &String,
+    the_vector_to_remove_from_the_save_var: &mut Vec<String>,
+) -> String {
     let length_of_vec = the_vector_to_remove_from_the_save_var.len();
-    
-    for i in 0..length_of_vec{
+
+    for i in 0..length_of_vec {
         if &the_vector_to_remove_from_the_save_var[i] == what_to_remove {
             the_vector_to_remove_from_the_save_var.remove(i);
             return "Done!".to_string();
-            
         }
     }
     dbg!(the_vector_to_remove_from_the_save_var);
-    return "err in remove func (maybe no input 🤷‍♂️)".to_string();
-    
+    "err in remove func (maybe no input 🤷‍♂️)".to_string()
 }
-
-fn add (x: &Vec<&str>) -> String {
+//-----------------------------------------Add---------------------------------------
+fn add(x: &[&str]) -> String {
     let mut result = String::new();
     for i in &x[1..] {
-        result.push_str(&i);
-        result.push_str(" ");
+        result.push_str(i);
+        result.push(' ');
     }
     result.pop();
 
-    return result;
+    result
 }
 
 fn main() {
-    let mut save:Vec<String> = vec![];
+    let mut save: Vec<String> = vec![];
     loop {
         let input: String = input();
         let parser_out: Vec<&str> = parser(&input);
-        let ref_parser_out: &Vec<&str> = &parser_out;
+        
 
-        let command: Result<i8, String> = command_detechtor(&ref_parser_out);
+        let command: Result<i8, String> = command_detechtor(&parser_out);
 
         if command == Ok(1) {
             save.push(add(&parser_out));
@@ -79,8 +72,6 @@ fn main() {
             remove(&parser_out[1..].join(" "), &mut save);
         }
 
-        dbg!(&command);
-        dbg!(&save);
     }
 
     // match command {
@@ -88,12 +79,6 @@ fn main() {
     // };
 
     // dbg!(&save);
-    
 
-
-    
     // println!("{:?}", parser_out);
-
-
 }
-
